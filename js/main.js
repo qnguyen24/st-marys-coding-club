d661219a46373241ed0cadde36175c7fdb7c3012
Quang <quang.nguyen1@ucalgary.ca>
2020-10-18 19:21:25 -0600

    fresh-homerun-style-site-rollout

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
 });
 
 
-//for changing horizontal navbar css on scroll
+// for changing horizontal navbar css on scroll
 $(function () {
   $(document).scroll(function () {
     var $nav = $("#navbar-wrapper");
@@ -60,6 +60,60 @@ $(function () {
 
 
 //trigger navigation curtain animation
-  
-  
 
+
+
+// hamburger animation https://jonsuh.com/hamburgers/
+  // Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // var navbar = document.querySelector(".navbar");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // hamburger.classList.toggle("border-bottom");
+    // Do something else, like open/close menu
+  });
+
+
+
+  // orbiting logos https://stackoverflow.com/questions/39020670/rotate-objects-around-circle-using-css
+
+  var radius = 100; // adjust to move out items in and out 
+var fields = $('.item'),
+  container = $('#container'),
+  width = container.width(),
+  height = container.height();
+var angle = 0,
+  step = (2 * Math.PI) / fields.length;
+fields.each(function() {
+  var x = Math.round(width / 2 + radius * Math.cos(angle) - $(this).width() / 2);
+  var y = Math.round(height / 2 + radius * Math.sin(angle) - $(this).height() / 2);
+  if (window.console) {
+    console.log($(this).text(), x, y);
+  }
+  $(this).css({
+    left: x + 'px',
+    top: y + 'px'
+  });
+  angle += step;
+});
+
+
+
+
+// accordion animation
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
